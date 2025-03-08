db7df0a91ef7e4171bfc74178e449b49538f92a0
hyde-mary <hydemaryalzbayne@gmail.com>
2025-03-08 11:52:37 +0800

    feat: add bottom sheet for custom header

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,66 +7,57 @@ import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
 import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 import { Ionicons } from "@expo/vector-icons";
+import CustomHeader from "@/components/headers/CustomHeader";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: useClientOnlyValue(false, true),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="home-outline" size={25} color={color} />
-          ),
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+    <BottomSheetModalProvider>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          headerShown: useClientOnlyValue(false, true),
         }}
-      />
-      <Tabs.Screen
-        name="dashboard"
-        options={{
-          title: "Dashboard",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="grid-outline" size={25} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="ai-suggestion"
-        options={{
-          title: "AI",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="nutrition-outline" size={25} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="person-outline" size={25} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="home-outline" size={25} color={color} />
+            ),
+            header: () => <CustomHeader />,
+          }}
+        />
+        <Tabs.Screen
+          name="dashboard"
+          options={{
+            title: "Dashboard",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="grid-outline" size={25} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="ai-suggestion"
+          options={{
+            title: "AI",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="nutrition-outline" size={25} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person-outline" size={25} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </BottomSheetModalProvider>
   );
 }
---
+++ components/headers/CustomHeader.tsx
@@ -0,0 +1,145 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Image,
+} from "react-native";
+import React, { useRef } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Link, useNavigation } from "expo-router";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Colors from "@/constants/Colors";
+import BottomSheet from "../sheets/BottomSheet";
+
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons
+          style={styles.searchIcon}
+          name="search-outline"
+          size={20}
+          color={Colors.medium}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Restaurants, groceries, dishes"
+        />
+      </View>
+      <Link href={"/(modal)/filter"} asChild>
+        <TouchableOpacity style={styles.optionButton}>
+          <Ionicons name="options-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </Link>
+    </View>
+  </View>
+);
+
+const CustomHeader = () => {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+  const insets = useSafeAreaInsets();
+  const navigation = useNavigation();
+
+  const openModal = () => {
+    bottomSheetRef.current?.present();
+  };
+
+  return (
+    <SafeAreaView style={[styles.safeArea, { paddingTop: insets.top }]}>
+      <BottomSheet ref={bottomSheetRef} />
+      <View style={styles.container}>
+        <TouchableOpacity onPress={openModal}>
+          <Image
+            style={styles.bike}
+            source={require("@/assets/images/bike.png")}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.titleContainer} onPress={openModal}>
+          <Text style={styles.title}>Delivery · Now</Text>
+          <View style={styles.locationName}>
+            <Text style={styles.subtitle}>Metro Manila</Text>
+            <Ionicons name="chevron-down" size={20} color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      <SearchBar />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 0, // Remove extra space at the top
+    backgroundColor: "#fff",
+  },
+  container: {
+    height: 60,
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  bike: {
+    width: 30,
+    height: 30,
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  locationName: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  searchContainer: {
+    backgroundColor: "#fff",
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    padding: 10,
+    color: Colors.mediumDark,
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
+});
+
+export default CustomHeader;
